6e928aebf24d626a941586fff240c44ad1648045
subeen-me <subeen.me@gmail.com>
2022-02-27 18:27:23 +0900

    MemberServiceTest

--- src/main/java/jpabook/jpashop/MemberRepository2.java
+++ src/main/java/jpabook/jpashop/MemberRepository2.java
@@ -6,7 +6,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 @Repository
-public class MemberRepository {
+public class MemberRepository2 {
 
     @PersistenceContext
     EntityManager em;
