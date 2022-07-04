4b2db81e994a331410cc17e3d8df40708642fbdd
subeen-me <subeen.me@gmail.com>
2022-07-04 23:20:09 +0900

    batch_size 설정

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -104,8 +104,20 @@ public class OrderRepository {
                         " join fetch o.member m" +
                         " join fetch o.delivery d", Order.class
         ).getResultList();
-
     }
+
+    //페치조인 + 페이징
+    public List<Order> findAllWithMemberDelivery(int offset, int limit) {
+        return em.createQuery(
+                "select o from Order o" +
+                        " join fetch o.member m" +
+                        " join fetch o.delivery d", Order.class)
+                .setFirstResult(offset)
+                .setMaxResults(limit)
+                .getResultList();
+    }
+
+
 
     public List<OrderSimpleQueryDto> findOrderDtos() {
         return em.createQuery(
@@ -125,6 +137,8 @@ public class OrderRepository {
                 " join fetch oi.item i", Order.class)
                 .getResultList();
     }
+
+
 
        /*
     public List<Order> findAll(OrderSearch orderSearch) {
